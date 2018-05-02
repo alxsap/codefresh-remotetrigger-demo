a4511e440359e121fd76e98a0a50d78dc6d0083e
Alexander Link <alexander.link@sap.com>
2018-05-02 11:13:05 +0200

    trigger remote jenkins job

--- remoteTrigger.sh
+++ remoteTrigger.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-echo Hello World
+if [ -z "$JENKINS_JOB_URL" ]; then
+    echo "Need to set JENKINS_JOB_URL"
+    exit 1
+fi  
+
+if [ -z "$JENKINS_USER" ]; then
+    echo "Need to set JENKINS_USER"
+    exit 1
+fi  
+
+if [ -z "$JENKINS_PASSWORD" ]; then
+    echo "Need to set JENKINS_PASSWORD"
+    exit 1
+fi  
+
+
+echo Triggering remote job $JENKINS_JOB_URL
+
+queueUrl=`curl --request POST --basic --user $JENKINS_USER:$JENKINS_PASSWORD --silent -D - '$JENKINS_JOB_URL/build' | grep Location: | sed 's/Location: //'`
+buildUrl=`curl --request GET --basic --user $JENKINS_USER:$JENKINS_PASSWORD  --silent "$queueUrl/api/xml?xpath=leftItem/executable/url" | sed 's/<url>//' | sed 's/<\/url>//'`
+
+cf_export buildUrl=$buildUrl
